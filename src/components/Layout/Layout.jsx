ffc78bcb9285d3a62a53b6d13f1ca5963964f398
Oleg-Kaniuk <129388390+Oleg-Kaniuk@users.noreply.github.com>
2023-11-14 17:46:52 +0200

    layout, routers

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom"
+import { Suspense } from "react"; 
+import { MyAppBar } from "components/MyAppBar/MyAppBar";
+    
+export const Layout = () => {
+    return <>
+        <header>
+            <MyAppBar />
+        </header>
+        <main>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Outlet />
+            </Suspense>
+        </main>
+    </>
+};
